d3ca92365e156f4efbceed961468611b2fc9a6f7
Alibek Kaliyev <alk224@lehigh.edu>
2021-03-30 16:28:48 -0400

    Added Android Configuration for Firebase
    Needs iOS Configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lu_interactive_campus_map_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    // await tester.pumpWidget(MyApp()); // commented this
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
